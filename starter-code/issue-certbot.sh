0d48900fc5b15c40908b4224b13d6ad178b12f29
GKephart <gkephart@cnm.edu>
2019-05-30 16:28:00 -0600

    added broken files to fix the answer key.

--- starter-code/issue-certbot.sh
+++ starter-code/issue-certbot.sh
@@ -7,5 +7,5 @@ certbot/certbot \
 certonly --webroot \
 --webroot-path=/data/letsencrypt \
 --agree-tos  --no-eff-email \
---email your@email.you
--d dont-blindly-copy-past.face-palm -d www.dont-blindly-copy-past.face-palm
+--email your@email.you \
+ -d justsmarty.club -d www.justsmarty.club
